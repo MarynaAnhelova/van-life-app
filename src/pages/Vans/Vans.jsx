b0c59b30d4bbde3e402196a3581fd6a98302dfa9
MarynaAnhelova <marina.anhelova@gmail.com>
2025-01-10 16:21:03 -0800

    setSearchParams filter buttons

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -31,38 +31,32 @@ export default function Vans() {
     ))
 
     /**
-     * Challenge: add links to filter the vans by type. Use a hard-coded
-     * `to` string like we just practiced. The types are "simple",
-     * "luxury", and "rugged".
-     *
-     * For now, give the Links a className of `van-type simple` (and
-     * manually replace "simple" with "luxury" and "rugged" for
-     * the Links that filter by those types.)
-     *
-     * Include a Link to clear the filters. Its className should be
-     * `van-type clear-filters`
+     * Challenge: change the Links to buttons and use the
+     * setSearchParams function to set the search params
+     * when the buttons are clicked. Keep all the classNames
+     * the same.
      */
 
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <Link
-                    to="?type=simple"
+                <button
+                    onClick={() => setSearchParams({type: "simple"})}
                     className="van-type simple"
-                >Simple</Link>
-                <Link
-                    to="?type=luxury"
+                >Simple</button>
+                <button
+                    onClick={() => setSearchParams({type: "luxury"})}
                     className="van-type luxury"
-                >Luxury</Link>
-                <Link
-                    to="?type=rugged"
+                >Luxury</button>
+                <button
+                    onClick={() => setSearchParams({type: "rugged"})}
                     className="van-type rugged"
-                >Rugged</Link>
-                <Link
-                    to="."
+                >Rugged</button>
+                <button
+                    onClick={() => setSearchParams({})}
                     className="van-type clear-filters"
-                >Clear filter</Link>
+                >Clear filter</button>
 
             </div>
             <div className="van-list">
