530a27c18bf76c8c4e72bed6e0c4559a1ed8018f
MarynaAnhelova <marina.anhelova@gmail.com>
2025-01-17 14:00:25 -0800

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import NotFound from "./pages/NotFound"
+import Login from "./pages/Login"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 import Error from "./components/Error"
@@ -30,6 +31,10 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route
+            path="login"
+            element={<Login />}
+        />
         <Route
             path="vans"
             element={<Vans />}
