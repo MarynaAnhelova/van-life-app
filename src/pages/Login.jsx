37e7e06fcddfd71e84b1127d59b0fd1e353b1e1a
MarynaAnhelova <marina.anhelova@gmail.com>
2025-01-23 12:44:09 -0800

    send the user to the /host route after they successfully log in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import {
     useLoaderData,
     useNavigate,
-    redirect,
-    Form
+    Form,
+    redirect
 } from "react-router-dom"
 import { loginUser } from "../api"
 
@@ -17,8 +17,7 @@ export async function action({ request }) {
     const password = formData.get("password")
     const data = await loginUser({ email, password })
     localStorage.setItem("loggedin", true)
-
-    return null
+    return redirect("/host")
 }
 
 export default function Login() {
