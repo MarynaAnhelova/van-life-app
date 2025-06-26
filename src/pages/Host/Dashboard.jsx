65c75ae0b33da6545da6002362de9e58a844bf45
MarynaAnhelova <marina.anhelova@gmail.com>
2025-06-26 15:45:32 -0700

    dashboard fontawesome icon

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,12 +1,65 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
+import { Link, defer, Await, useLoaderData } from "react-router-dom"
+import { getHostVans } from "../../api"
+import { requireAuth } from "../../utils"
+
+
+export async function loader({ request }) {
+    await requireAuth(request)
+    return defer({ vans: getHostVans() })
+}
 
 export default function Dashboard() {
+    const loaderData = useLoaderData()
+
+    function renderVanElements(vans) {
+        const hostVansEls = vans.map((van) => (
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+                <Link to={`vans/${van.id}`}>View</Link>
+            </div>
+        ))
+
+        return (
+            <div className="host-vans-list">
+                <section>{hostVansEls}</section>
+            </div>
+        )
+    }
+
     return (
         <>
-            <h1>Dashboard goes here</h1>
-            <Outlet />
+            <section className="host-dashboard-earnings">
+                <div className="info">
+                    <h1>Welcome!</h1>
+                    <p>Income last <span>30 days</span></p>
+                    <h2>$2,260</h2>
+                </div>
+                <Link to="income">Details</Link>
+            </section>
+            <section className="host-dashboard-reviews">
+                <h2>Review score</h2>
+                <svg className="icon-star-review" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                <p>
+                    <span>5.0</span>/5
+                </p>
+                <Link to="reviews">Details</Link>
+            </section>
+            <section className="host-dashboard-vans">
+                <div className="top">
+                    <h2>Your listed vans</h2>
+                    <Link to="vans">View all</Link>
+                </div>
+                <React.Suspense fallback={<h3>Loading...</h3>}>
+                    {/*<Await resolve={loaderData.vans}>{renderVanElements}</Await>*/}
+                </React.Suspense>
+            </section>
         </>
     )
 }
+
 
